1255e901d54958c587169e4299055de3c3d63cd8
Vladyslav Dikhtiaruk <vladyslav.dikhtiaruk@hva.nl>
2023-11-02 17:47:19 +0100

    changed ListOfMovies model

--- app/src/main/java/nl/hva/madlevel4task2/data/model/ListOfMovies.kt
+++ app/src/main/java/nl/hva/madlevel4task2/data/model/ListOfMovies.kt
@@ -1,8 +1,14 @@
 package nl.hva.madlevel4task2.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ListOfMovies(
+    @SerializedName("page")
     val page: Int,
+    @SerializedName("results")
     val results: List<Movie>,
+    @SerializedName("total_pages")
     val total_pages: Int,
+    @SerializedName("total_results")
     val total_results: Int
 )
