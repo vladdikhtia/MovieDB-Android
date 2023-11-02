ffba472d33b46d0eb1e6e7786c1df430455c5481
Vladyslav Dikhtiaruk <vladyslav.dikhtiaruk@hva.nl>
2023-11-02 17:48:28 +0100

    changed Movie model

--- app/src/main/java/nl/hva/madlevel4task2/data/model/Movie.kt
+++ app/src/main/java/nl/hva/madlevel4task2/data/model/Movie.kt
@@ -1,11 +1,21 @@
 package nl.hva.madlevel4task2.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Movie(
+//The value in @SerializedName should comply with the json field name.
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("backdrop_path")
     val backdrop_path: String,
+    @SerializedName("poster_path")
     val poster_path: String,
+    @SerializedName("title")
     val title: String,
+    @SerializedName("release_date")
     val release_date: String,
+    @SerializedName("overview")
     val overview: String,
+    @SerializedName("vote_average")
     val vote_average: Double,
 )
