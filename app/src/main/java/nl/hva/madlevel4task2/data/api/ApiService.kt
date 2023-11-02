d2109c3d7b203d2f77cf79c3d809c185449b3cc0
Vladyslav Dikhtiaruk <vladyslav.dikhtiaruk@hva.nl>
2023-11-02 18:00:04 +0100

    changed ApiService

--- app/src/main/java/nl/hva/madlevel4task2/data/api/ApiService.kt
+++ app/src/main/java/nl/hva/madlevel4task2/data/api/ApiService.kt
@@ -2,9 +2,16 @@ package nl.hva.madlevel4task2.data.api
 
 import nl.hva.madlevel4task2.data.model.ListOfMovies
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface ApiService {
+    @Headers(
+        "accept : application/json",
+        "Authorization : Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNWEwYTcyNDYyY2E2NGYwMDcxOWE1N2JhNDYxNTdjYyIsInN1YiI6IjY1NDM3ZGJmZTFhZDc5MDBlYTU3ZDQxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QizJiZ72zlcyaG6yDHEvUysEZAd6qLMczgYmbRQ7wAU"
+    )
+
     // The GET method needed to retrieve
-    @GET("search/movie")
-    suspend fun getMovies() :ListOfMovies
+    @GET("3/search/movie")
+    suspend fun getMovies(@Query("query") query: String) :ListOfMovies
 }
